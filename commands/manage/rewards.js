88f87eeb7c793c1b11cf8e36b285db96d907c92f
Antikore <koopatropa8@hotmail.com>
2022-06-12 14:40:56 +0200

    Added rewards

--- commands/manage/rewards.js
+++ commands/manage/rewards.js
@@ -1,14 +1,30 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
-const { color, emoji: emojis, parseUse, emojir } = require('../../globals');
+const { color, emoji: emojis, parseUser } = require('../../globals');
 
 module.exports = {
 	permissions: ['manage_rewards'],
 	data: new SlashCommandBuilder()
 		.setName('rewards')
 		.setDescription('Create a new trophy for your server.')
-		.addStringOption(option => option.setName('name').setDescription('The name of the trophy.').setRequired(true))
-		.addStringOption(option => option.setName('description').setDescription('Description for the trophy').setRequired(false)),
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('add')
+				.setDescription('Add permissions to a role.')
+				.addRoleOption(option => option.setName('target').setDescription('Which role you want to add as reward.').setRequired(true))
+				.addIntegerOption(option => option.setName('requirement').setDescription('How much score the user will require to get this role.').setRequired(true))
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('remove')
+				.setDescription('Remove a role reward from your server.')
+				.addRoleOption(option => option.setName('role').setDescription('Which role you want to remove from rewards').setRequired(true))
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('list')
+				.setDescription('List of reward roles.')
+		),
 
 	async run (interaction) {
 	}
