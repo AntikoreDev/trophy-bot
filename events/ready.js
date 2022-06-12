af9a5513f87f2372ad74b0654232ab1156575492
Antikore <koopatropa8@hotmail.com>
2022-06-12 19:24:23 +0200

    Fixed some issues

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 // This event runs whenever the bot is ready to start working!
 const { fetchModules, changeActivity } = require('../globals.js');
 const path = require('path');
+const Discord = require('discord.js');
 
 module.exports = {
 	name: 'ready',
@@ -10,15 +11,16 @@ module.exports = {
 		// Import all the modules
 		client.commands 	= await fetchModules(path.join(__dirname, '../commands'), '.js', true);
 		client.languages 	= await fetchModules(path.join(__dirname, '../locale/languages'));
-		client.settings 	= await fetchModules(path.join(__dirname, '../settings'));
-
+		
 		// Set the bot's status
 		console.log(`=: Bot Initialised :=`);
 		console.log(`Name: ${client.user.username}#${client.user.discriminator}`);
 		console.log(`ID: ${client.user.id}`);
 
+		client.cooldowns = new Discord.Collection();
+
 		// Set the client user's activity.
-		client.user.setActivity(`${client.db.bot.get(`data.trophiesAwarded`, 0)} awarded trophies!`, { type: 'WATCHING' });
+		await client.user.setActivity(`${client.db.bot.get(`data.trophiesAwarded`, 0)} awarded trophies!`, { type: 'WATCHING' });
 
 		changeActivity(client);
 
