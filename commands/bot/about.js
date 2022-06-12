63950da1c3fe6a3c98d6776b1be89d276510e8ef
Antikore <koopatropa8@hotmail.com>
2022-06-12 11:54:13 +0200

    Update about.js

--- commands/bot/about.js
+++ commands/bot/about.js
@@ -14,9 +14,9 @@ module.exports = {
 		embed.setTitle(`About Trophy Bot :trophy:`);
 		embed.setDescription(
 			`This bot allows you to create, customize and award trophies to people in your server\n\n` +
-			`You can find us on [Github]()\n` +
-			`Liking the bot? Consider supporting us on [Ko-fi](https://ko-fi.com/antikore)\n\n` +
-			`Bot created by Antikore`
+			`You can find us on [Github](https://github.com/Aidanete/trophy-bot)\n` +
+			`**Liking the bot?** Consider supporting us on [Ko-fi](https://ko-fi.com/antikore)\n\n` +
+			`Bot created by *@Antikore#9357*`
 		);
 
 		embed.setThumbnail(interaction.client.user.displayAvatarURL());
