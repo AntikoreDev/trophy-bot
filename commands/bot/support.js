d0899286ad6341a931b7f7e1c3bcc85eb528d958
Antikore <koopatropa8@hotmail.com>
2022-06-13 15:44:11 +0200

    Added support command

---
+++ commands/bot/support.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require('discord.js');
+const { color } = require('../../globals');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('support')
+		.setDescription('You need extra help? Join our support server.'),
+		
+	async run (interaction) {
+		
+		const embed = new Discord.MessageEmbed();
+
+		embed.setColor(color.main);
+		embed.setThumbnail(interaction.client.user.displayAvatarURL());
+		embed.setTitle(`:question: You need support?`);
+		embed.setDescription(
+			`You can get help in our [Support Server](https://discord.gg/kNmgU44xgU)\n\n` +
+			`You can also report bugs on [Github](https://github.com/Aidanete/trophy-bot/issues)\n` +
+			`Or suggest stuff with **/suggest**.`
+		)
+
+		if (!interaction) return;
+		interaction.reply({
+			embeds: [embed],
+			ephemeral: true
+		});
+	}
+}
