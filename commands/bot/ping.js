3ebab236c9a1fb9c79ffdf42a07e2050aa2ab7e1
Antikore <koopatropa8@hotmail.com>
2022-06-15 16:04:09 +0200

    Removed latency because doesn't work

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -13,15 +13,10 @@ module.exports = {
 
 		// Get the rounded websocket ping
 		const api = Math.round(interaction.client.ws.ping);
-
-		// Only divide by 1000 if the system os is windows (For some fucking reason)
-		const div = (process.platform === "win32" ? 1000 : 1);
-		const latency = -(Math.round((Date.now() - interaction.createdTimestamp) / div) - 1);
 		
 		embed.setColor(color.main);
 		embed.setDescription(
 			`\n:robot: **Discord API:** ${api} ms` +
-			`\n:speech_balloon: **Bot Latency:** ${latency} ms` +
 			`\n\nThis is bot ping, not yours :).`
 		);
 
