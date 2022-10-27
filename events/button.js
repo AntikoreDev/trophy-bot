e5d7936ca9063635f89b30db241539c57e056c10
Antikore <koopatropa8@hotmail.com>
2022-10-27 17:18:16 +0200

    Added button event

---
+++ events/button.js
@@ -0,0 +1,34 @@
+const { forgetMe } = require('../globals');
+// Yeah, just because working correctly with awaitMessageComponent is so maisntream.
+module.exports = {
+	name: 'interactionCreate',
+	once: false,
+	async run (interaction) {
+		if (!interaction.inGuild()) return;
+		if (!interaction.isButton()) return;
+
+		await interaction.deferReply({ ephemeral: true, fetchReply: true });
+
+		if (interaction.guild.ownerId == interaction.member.user.id){
+			if (interaction.customId == "forgetmeproceed"){
+				await interaction.editReply({
+					content: 'Okay! Thanks for using Trophy Bot. See ya next time :)',
+				});
+
+				await forgetMe(interaction.client, interaction.guild);
+				return;
+			}else if (interaction.customId == "forgetmenope"){
+				await interaction.editReply({
+					content: 'Operation stopped',
+				});
+
+			}
+		}
+		
+		if (!interaction.replied){
+			await interaction.editReply({
+				content: 'Not replied',
+			});
+		}
+	}
+};
