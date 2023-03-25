5ca7d849e51e0f099ba5acb28cd9dc3def403eec
Antikore <koopatropa8@hotmail.com>
2023-03-25 16:48:00 +0100

    Update edit.js

--- commands/manage/edit.js
+++ commands/manage/edit.js
@@ -132,11 +132,13 @@ module.exports = {
 		if (emoji != current.emoji) 		changes.push(`**Emoji:** ${current.emoji} > ${emoji}`);
 		// if (value != current.value) 		changes.push(`**Value:** ${current.value} > ${value}`);
 		if (dedic) 							changes.push(`**Dedication:** ${current.dedication.name ? current.dedication.name : `No dedication`} > ${dedication.name}`);
+		if (details != current.details) 	changes.push(`**Details changed**`);
 		if (image != current.image) 		changes.push(`**Changed image**`);
+		
 
 		if (!changes.length){
 			embed.setColor(color.error);
-			embed.setDescription(`${emojis.error} No changes were made to **${current}**`);
+			embed.setDescription(`${emojis.error} No changes were made to **${current.name}**`);
 
 			return interaction.editReply({
 				embeds: [embed]	
