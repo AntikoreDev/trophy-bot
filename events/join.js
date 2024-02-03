2faf80d249a93e2bee7b10d8ab82f05e4a50453b
Antikore <koopatropa8@hotmail.com>
2024-02-03 18:04:23 +0100

    ⚰️ Remove already achieved milestone

--- events/join.js
+++ events/join.js
@@ -1,5 +1,4 @@
 const { EmbedBuilder } = require('discord.js');
-const { color } = require('../globals');
 
 // Note from the developer, I hate interactions and the whole slash command system.
 module.exports = {
@@ -9,30 +8,6 @@ module.exports = {
 		
 		console.log(`Joined a new guild!`);
 		await guild.members.fetch();
-
-		// In case someone is reading this...
-		// Just a friendly reminder for myself to ha- love Discord API <3
-		if (guild?.client?.guilds?.cache?.size == 75){
-
-			const first = guild.client.db.bot.get(`data.milestone`) ?? null;
-			if (first == null){
-
-				const embed = new EmbedBuilder();
-
-				embed.setColor(color.success);
-				embed.setTitle(`:tada: Trophy Bot got to 75 servers!`);
-				embed.setDescription(`You should be proud of yourself! Now you should waitlist for the server members intent and verification. yay!`);
-
-				await guild.client.errorChannel.send(
-					{
-						content: `<@353998390734094346>`,
-						embeds: [embed],
-					}
-				);
-
-				guild.client.db.bot.set(`data.milestone`, true);
-			}
-		}
 		
 	}
 }
