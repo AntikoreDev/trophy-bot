b3fa1049e7f711dac87729983a04964d7c4c175e
Antikore <koopatropa8@hotmail.com>
2024-02-03 18:00:42 +0100

    🌐 Add some locale entries

--- locale/translations/en.js
+++ locale/translations/en.js
@@ -1,4 +1,32 @@
 module.exports = {
 	id: 'en',
-	entries: {}
+	entries: {
+		error_generic_title: "Error!",
+		error_generic_trophy_not_found: "Could not find a trophy with the name or ID of \`%filter%\`",
+		error_delete_generic: "Couldn't delete trophy with name or ID of \`%filter%\`",
+		error_award_amount: "You can only award between **%min%** and **%max%** trophies per command.",
+		
+		error_award_generic: "Couldn't successfully award those trophies to the user",
+	
+		error_revoke_generic: "Couldn't successfully revoke those trophies to the user",
+		error_revoke_amount: "You can only revoke between **%min%** and **%max%** trophies per command.",
+		error_revoke_no_trophies: "%user% has no awards for %trophy%",
+
+		error_clear_generic: "Couldn't clear the trophies of that user",
+		
+		error_language_not_found: "Language for \`%filter%\` not found",
+
+		success_generic_title: "Done!",
+		success_language_changed: "Language was changed to %language%",
+
+		command_about_title: "About Trophy Bot",
+		command_about_desc: "This bot allows you to create, customize and award trophies to people in your server",
+		command_about_desc2: "To get started use %cmd% for a few basic info and commands",
+		command_about_github: "You can look for the source code at [Github](%link%)",
+		command_about_kofi: "**Liking the bot?** Consider supporting us on [Ko-fi](%link%)",
+		command_about_support: "**Need help?** Join the [Support Server](%link%)",
+		command_about_author: "Bot created by %user%",
+		
+		command_support_title: "You need support?",
+	}
 }
--- locale/translations/es.js
+++ locale/translations/es.js
@@ -1,4 +1,19 @@
 module.exports = {
 	id: 'es',
-	entries: {}
+	entries: {
+		error_generic_title: "¡Error!",
+
+		success_generic_title: "¡Hecho!",
+		success_language_changed: "El idioma fue cambiado a %language%",
+
+		command_about_title: "¿Qué es Trophy Bot?",
+		command_about_desc: "Este bot te permite crear, personalizar y dar trofeos a los usuarios de tu servidor.",
+		command_about_desc2: "Puedes utilizar %cmd% para ver más información y algunos comandos.",
+		command_about_github: "Puedes mirar el código fuente en [Github](%link%)",
+		command_about_kofi: "**¿Disfrutando del bot?** Considera apoyarnos através de [Ko-fi](%link%)",
+		command_about_support: "**¿Necesitas ayuda?** Únete a nuestro [servidor de soporte](%link%)",
+		command_about_author: "Bot creado por %user%",
+		
+		command_support_title: "¿Necesitas ayuda?"
+	}
 }
