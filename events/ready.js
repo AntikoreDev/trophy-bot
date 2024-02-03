b6e59dd03d20133655c47389b2912c6cee9eefb3
Antikore <koopatropa8@hotmail.com>
2024-02-03 18:03:40 +0100

    🔊 Update logs

--- events/ready.js
+++ events/ready.js
@@ -9,20 +9,23 @@ module.exports = {
 	once: true,
 	async run (client) {
 
+		console.log(`[Trophy Bot] Fetching dump channels...`);
 		try {
 			client.errorChannel = await client.channels.fetch("985869722199416862").then();
 			client.suggestionChannel = await client.channels.fetch("985872094153830400").then();
+		} catch (e) {
+			console.log(`[Trophy Bot] ERROR: Couldn't fetch error channel`);
 		}
-		catch {}
+
+		console.log(`[Trophy Bot] Reading commands and locales...`);
 
 		// Import all the modules
 		client.commands 	= await fetchModules(path.join(__dirname, '../commands'), '.js', true);
 		client.languages 	= await fetchModules(path.join(__dirname, '../locale/languages'));
 		
 		// Set the bot's status
-		console.log(`=: Bot Initialised :=`);
-		console.log(`Name: ${client.user.username}#${client.user.discriminator}`);
-		console.log(`ID: ${client.user.id}`);
+		console.log(`[Trophy Bot] Done!`);
+		console.log(`[Trophy Bot] Running as ${client.user.username}#${client.user.discriminator} with ID ${client.user.id}`);
 
 		client.cooldowns = new Discord.Collection();
 
