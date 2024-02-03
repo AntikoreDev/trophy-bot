2666bf2a35f9589596ce2f76660ecd1a72421116
Antikore <koopatropa8@hotmail.com>
2024-02-03 18:06:46 +0100

    🍻 Update command handler

--- events/command.js
+++ events/command.js
@@ -1,3 +1,4 @@
+const Database = require("../commons/database");
 const { getServer, anyIn, color, emoji, isDev, AttemptToFetchUsers, imsafeWarning } = require("../globals");
 const { EmbedBuilder } = require("discord.js");
 
@@ -7,8 +8,6 @@ module.exports = {
 	async run (interaction) {
 
 		if (!interaction.inGuild()) return;
-
-		// If it's not a command, then WHY the heck is it here?
 		if (!interaction.isCommand()) return;
 
 		await interaction.deferReply();
@@ -16,37 +15,19 @@ module.exports = {
 		const client = interaction.client;
 		const guild = interaction.guild;
 
+		const server = await Database.checkGuild(guild.id);
 		const commandName = interaction.commandName;
-		
-		const data = await getServer(client, guild.id, guild);
-
+	
 		// Fetch how many users is the bot serving
-		AttemptToFetchUsers(client);
+		// AttemptToFetchUsers(client);
 
 		if (!client.commands) return;
 
 		const command = client.commands.get(commandName);
-		const user = await interaction.member.fetch();
-
-		const embed = new EmbedBuilder();
-
 		if (!command) return;
 
-		// Get roles from the user
-		const roles = user.roles.cache.map(role => role.id);
-		const isAdmin = (await client.channels.fetch(interaction.channelId)).permissionsFor(interaction.member).toArray().includes('ADMINISTRATOR');
-
-		if (command.permissions && !isDev(interaction.user.id)) {
-			const imsafe = client.db.guilds.get(`data.${guild.id}.imsafe`) ?? false;
-			if (!imsafe){
-				return await imsafeWarning(interaction);
-			}
-		}
-
-		if (!client.db.bot.has(`data.commands.${commandName}`)) client.db.bot.set(`data.commands.${commandName}`, 0);
-
-		client.db.bot.add(`data.commands.${commandName}`, 1);
-		client.db.bot.add(`data.commands.total`, 1);
+		const user = await interaction.member.fetch();
+		const embed = new EmbedBuilder();
 
 		try {
 			await command.run(interaction);
@@ -64,12 +45,12 @@ module.exports = {
 					embed.setColor(color.error);
 					embed.setDescription(`**Command:** \`${interaction.toString()}\`\n**Perpetrator:** \`${interaction.user.id}\`\n**Guild:** \`${interaction.guild.id}\``);
 					embed.addFields({ name: `Stacktrace`, value: `\`\`\`js\n${stack}\`\`\`` });
+					embed.setFooter({ text: `IUI: ${interaction.id}` });
 	
 					client.errorChannel.send({
 						embeds: [embed]
 					});
-				}
-				catch(err){
+				} catch(err){
 					console.log(err);
 					interaction.channel.send(`Error log could not be sent, dev should know about this...`);
 				}
@@ -80,7 +61,7 @@ module.exports = {
 
 			const errorEmbed = new EmbedBuilder();
 			errorEmbed.setDescription(`${emoji.error} There was an error while executing this command!\nYou can join our [support server](https://discord.gg/kNmgU44xgU) to report the issue`);
-			errorEmbed.setFooter({ text: 'Errors are automatically delivered to the developer, it may be fixed in about 1 to 24 hours, but reporting will help to quick the process' });
+			errorEmbed.setFooter({ text: `IUI: ${interaction.id} · Errors are automatically delivered to the developer, it may be fixed in about 1 to 24 hours, but reporting will help to quick the process` });
 			errorEmbed.setColor(color.error);
 
 			await interaction.editReply({ ephemeral: true, embeds: [errorEmbed] });
